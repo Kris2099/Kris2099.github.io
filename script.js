88d841826640251f769811ce2bc1c4e23fca4d8d
Jorge <jorgealejandroescobar0@gmail.com>
2023-02-14 14:42:25 -0500

    Final

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const heartContainer = document.getElementById("heart-container");
-
-for (let i = 0; i < 50; i++) {
-  const heart = document.createElement("div");
-  heart.classList.add("heart");
-  heart.style.left = `${Math.floor(Math.random() * 100)}%`;
-  heart.style.top = `${Math.floor(Math.random() * -100)}px`;
-  heart.style.animationDelay = `${Math.random() * 10}s`;
-  heartContainer.appendChild(heart);
-}
-
-const message = "Te quiero, Kaori <3";
 const messageContainer = document.getElementById("message-container");
-let i = 0;
+const playButton = document.getElementById("play-button");
 
-const addLetter = () => {
-  if (i < message.length) {
-    messageContainer.innerHTML += message[i];
-    i++;
-    setTimeout(addLetter, 300);
+const startAnimation = () => {
+  for (let i = 0; i < 50; i++) {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.style.left = `${Math.floor(Math.random() * 100)}%`;
+    heart.style.top = `${Math.floor(Math.random() * -100)}px`;
+    heart.style.animationDelay = `${Math.random() * 10}s`;
+    heartContainer.appendChild(heart);
+    const audio = new Audio("song.mp3");
+    audio.autoplay = true;
+    audio.loop = true;
+    audio.volume = 0.1;
   }
+  
+  let i = 0;
+  const message = "Te quiero, Kaori <3";
+  
+  const addLetter = () => {
+    if (i < message.length) {
+      messageContainer.innerHTML += message[i];
+      i++;
+      setTimeout(addLetter, 300);
+    }
+  };
+  
+  addLetter();
+  
+  const audio = new Audio("song.flac");
+  audio.autoplay = true;
+  audio.loop = true;
+  audio.volume = 0.5;
+
+  playButton.style.display = "none";
 };
 
-addLetter();
+playButton.addEventListener("click", startAnimation);
